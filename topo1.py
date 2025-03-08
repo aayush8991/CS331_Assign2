9950887c89aafba86d3144a2b645bf1794315274
Aayush <137766962+aayush8991@users.noreply.github.com>
2025-03-08 21:14:53 +0530

    Add files via upload

---
+++ topo1.py
@@ -0,0 +1,49 @@
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.cli import CLI
+from mininet.log import setLogLevel
+from mininet.link import TCLink
+from mininet.node import RemoteController
+
+class MyTopo(Topo):
+    def __init__(self):
+        Topo.__init__(self)
+
+        # Add hosts
+        h1 = self.addHost('h1')
+        h2 = self.addHost('h2')
+        h3 = self.addHost('h3')
+        h4 = self.addHost('h4')
+        h5 = self.addHost('h5')
+        h6 = self.addHost('h6')
+        h7 = self.addHost('h7')
+
+        # Add switches
+        s1 = self.addSwitch('s1')
+        s2 = self.addSwitch('s2')
+        s3 = self.addSwitch('s3')
+        s4 = self.addSwitch('s4')
+
+        # Add links
+        self.addLink(h1, s1, cls=TCLink, bw=10)
+        self.addLink(h2, s1, cls=TCLink, bw=10)
+        self.addLink(s1, s2, cls=TCLink, bw=10)
+        self.addLink(h3, s2, cls=TCLink, bw=10)
+        self.addLink(s2, s3, cls=TCLink, bw=10)
+        self.addLink(h4, s3, cls=TCLink, bw=10)
+        self.addLink(h5, s3, cls=TCLink, bw=10)
+        self.addLink(s3, s4, cls=TCLink, bw=10)
+        self.addLink(h6, s4, cls=TCLink, bw=10)
+        self.addLink(h7, s4, cls=TCLink, bw=10)
+
+def run():
+    topo = MyTopo()
+    net = Mininet(topo=topo, link=TCLink, controller=None)
+    ryu_controller = net.addController('ryu', controller=RemoteController, ip='127.0.0.1', port=6633)
+    net.start()
+    CLI(net)
+    net.stop()
+
+if __name__ == '__main__':
+    setLogLevel('info')
+    run()
